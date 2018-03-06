e0a2cc380407130257ebe722333bbbb63e8aaad7
Gharvhel Carre <gc2767@columbia.edu>
2018-03-05 22:19:00 -0500

    first commit

---
+++ image_similarity_matching.cpp
@@ -0,0 +1,19 @@
+
+#include <opencv2/opencv.hpp>
+#include "image_similarity_matching.h"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
+void die(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
---
+++ image_similarity_matching.h
@@ -0,0 +1,6 @@
+#ifndef __IMAGE_SIMILARITY_MATCHING_H_INCLUDED__
+#define __IMAGE_SIMILARITY_MATCHING_H_INCLUDED__
+
+void die(const char *msg);
+
+#endif
